90c4d29738d3f64934bbc9f9245609f77eeeae42
shumittaher <shumitt@gmail.com>
2023-04-02 22:33:28 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styles from './style'
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <div>
-          
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
       </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          stats
+          business
+          billing 
+          carddeal
+          testimonials
+          clients
+          CTA
+          Footer
+        </div>
+      </div>
+
     </div>
   )
 }
